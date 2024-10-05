608c7bcdde233a6b6d44a1ba9434d28869a1fc89
Pranav Vashi <neobuddy89@gmail.com>
2024-10-06 01:29:46 +0530

    DNM: crdroid: Bringup
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/crDroidSettingsLayout.java
+++ src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -42,15 +42,17 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.crdroid.settings.fragments.About;
+import com.crdroid.settings.fragments.QuickSettings;
+import com.crdroid.settings.fragments.StatusBar;
+/*
 import com.crdroid.settings.fragments.Buttons;
 import com.crdroid.settings.fragments.LockScreen;
 import com.crdroid.settings.fragments.Miscellaneous;
 import com.crdroid.settings.fragments.Navigation;
 import com.crdroid.settings.fragments.Notifications;
-import com.crdroid.settings.fragments.QuickSettings;
 import com.crdroid.settings.fragments.Sound;
-import com.crdroid.settings.fragments.StatusBar;
 import com.crdroid.settings.fragments.UserInterface;
+*/
 
 public class crDroidSettingsLayout extends SettingsPreferenceFragment {
 
@@ -131,15 +133,17 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
 
         @Override
         protected Void doInBackground(Void... params) {
+/*
             Buttons.reset(rContext);
             LockScreen.reset(rContext);
             Miscellaneous.reset(rContext);
             Navigation.reset(rContext);
             Notifications.reset(rContext);
-            QuickSettings.reset(rContext);
             Sound.reset(rContext);
-            StatusBar.reset(rContext);
             UserInterface.reset(rContext);
+*/
+            QuickSettings.reset(rContext);
+            StatusBar.reset(rContext);
             finish();
             startActivity(getIntent());
             return null;
@@ -166,6 +170,8 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             super(fm);
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
+            frags[2] = new About();
+/*
             frags[2] = new LockScreen();
             frags[3] = new Navigation();
             frags[4] = new Buttons();
@@ -173,7 +179,7 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             frags[6] = new Notifications();
             frags[7] = new Sound();
             frags[8] = new Miscellaneous();
-            frags[9] = new About();
+*/
         }
 
         @Override
@@ -197,6 +203,8 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         titleString = new String[] {
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
+            getString(R.string.about_crdroid)
+/*
             getString(R.string.lockscreen_title),
             getString(R.string.navigation_title),
             getString(R.string.button_title),
@@ -204,7 +212,7 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             getString(R.string.notifications_title),
             getString(R.string.sound_title),
             getString(R.string.misc_title),
-            getString(R.string.about_crdroid)
+*/
         };
         return titleString;
     }
